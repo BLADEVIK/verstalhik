ce172fc5af5de64380eb74570ec4d6dd84c001e3
Bladevik <Blade-85@inbox.ru>
2025-03-16 16:23:17 +0300

    анимация карточек

--- script.js
+++ script.js
@@ -1,10 +1,65 @@
-const items = document.querySelectorAll('.testimonial-item');
-let current = 0;
+// Инициализация начального времени: 0 часов, 10 минут, 0 секунд
+let hours = 0;
+let minutes = 10;
+let seconds = 0;
 
-function showItem(index) {
-    items.forEach((item, i) => {
-        item.style.display = i === index ? 'block' : 'none';
-    });
+// Функция для обновления отображаемого времени
+function updateDisplay() {
+    const timerElement = document.querySelector('.timer');
+    const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    timerElement.textContent = formattedTime;
 }
 
-showItem(current);
+// Функция для запуска таймера
+function startTimer() {
+    setInterval(() => {
+        // Если таймер достиг 00:00:00, сбрасываем его на 00:10:00
+        if (hours === 0 && minutes === 0 && seconds === 0) {
+            minutes = 10;
+            seconds = 0;
+        } else {
+            // Уменьшаем время
+            seconds--;
+            if (seconds < 0) {
+                seconds = 59;
+                minutes--;
+                if (minutes < 0) {
+                    minutes = 59;
+                    hours--;
+                }
+            }
+        }
+        // Обновляем отображение
+        updateDisplay();
+    }, 1000);
+}
+
+// Устанавливаем начальное значение на экране
+updateDisplay();
+// Запускаем таймер при загрузке страницы
+startTimer();
+// Открываем sidebar при адаптиве
+document.querySelector('.hamburger').addEventListener('click', () => {
+    document.querySelector('.sidebar').classList.toggle('show');
+  });
+// Изменяем цвета карточки при наведении
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+  const button = card.querySelector('.button');
+  const price = card.querySelector('.card-price')
+  const hiddenText = card.querySelector('.hidden-text');
+  button.addEventListener('mouseover', () => {
+    card.classList.add('hover-effect');
+    button.classList.add('hover-effect');
+    price.classList.add('hover-effect');
+    hiddenText.classList.add('visible');
+  });
+  
+  button.addEventListener('mouseout', () => {
+    card.classList.remove('hover-effect');
+    button.classList.remove('hover-effect');
+    price.classList.remove('hover-effect');
+    hiddenText.classList.remove('visible');
+  });
+});
